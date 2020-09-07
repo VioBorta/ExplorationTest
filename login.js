5de322ac07d217746887680842e973e77714db54
VioBorta <viorica.borta123@gmail.com>
2020-09-07 16:59:24 +0100

    commit

---
+++ login.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+const hey = document.getElementById("hey");
+const loginId = document.getElementById("loginId");
+const avatarID = document.getElementById("avatarID");
+const userLogged = document.getElementById("userLogged");
+
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+
+  if (username === "user" && password === "pass") {
+    window.localStorage.setItem(username, "user");
+    window.localStorage.setItem(password, "web_dev");
+    console.log("logged");
+    loginId.style.display = "none";
+    avatarID.style.display = "block";
+    loginForm.style.display="none"
+    userLogged.style.display = "block";
+  } else {
+    loginErrorMsg.style.opacity = 1;
+  }
+});
